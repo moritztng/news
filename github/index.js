7850ae5e16fd580a10474f72fa01a5d3d5c8e6b0
Moritz Thüning <moritz.thuening@gmail.com>
2022-09-12 13:44:22 +0200

    Create monorepo and finish github function

---
+++ github/index.js
@@ -0,0 +1,95 @@
+const functions = require('@google-cloud/functions-framework')
+const { BigQuery } = require('@google-cloud/bigquery')
+const { graphql } = require('@octokit/graphql')
+
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${process.env.GITHUB_TOKEN}`,
+  },
+})
+
+const QUERY = `
+  query ($searchQuery: String!, $cursor: String) {
+    search(query: $searchQuery, type: REPOSITORY, first: 100, after: $cursor) {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      edges {
+        node {
+          ... on Repository {
+            owner {
+              login
+            }
+            name
+            stargazerCount
+          }
+        }
+      }
+    }
+  }
+`
+
+async function fetchRepositories(
+  graphql,
+  minStars,
+  maxStars,
+  { repositories, cursor } = { repositories: [] }
+) {
+  const searchQuery = `is:public stars:${minStars}..${maxStars} sort:stars-asc`
+  const result = await graphql(QUERY, { cursor, searchQuery: searchQuery })
+  const resultRepositories = result.search.edges.map((edge) => edge.node)
+
+  repositories.push(...resultRepositories)
+
+  if (result.search.pageInfo.hasNextPage) {
+    await fetchRepositories(graphql, minStars, maxStars, {
+      repositories,
+      cursor: result.search.pageInfo.endCursor,
+    })
+  }
+
+  return repositories
+}
+
+const bigquery = new BigQuery()
+
+functions.http('getRepositories', async (req, res) => {
+  const maxStars = parseInt(process.env.MAX_STARS)
+  let minStars = parseInt(process.env.MIN_STARS)
+  let repositories = []
+  let repositoriesSlice = []
+  do {
+    repositoriesSlice = await fetchRepositories(
+      graphqlWithAuth,
+      minStars,
+      maxStars
+    )
+    repositories = repositories.concat(repositoriesSlice)
+    minStars = repositories[repositories.length - 1].stargazerCount
+  } while (repositoriesSlice.length >= 1000)
+  
+  repositories = repositories.filter(
+    (value, index, self) =>
+      index ===
+      self.findIndex(
+        (t) => t.owner.login === value.owner.login && t.name === value.name
+      )
+  )
+  
+  const time = bigquery.datetime(new Date().toISOString())
+  
+  console.log(repositories.length)
+
+  await bigquery
+    .dataset('github_repositories')
+    .table('repositories')
+    .insert(
+      repositories.map((repository) => ({
+        time: time,
+        owner: repository.owner.login,
+        name: repository.name,
+        stargazerCount: repository.stargazerCount,
+      }))
+    )
+})
