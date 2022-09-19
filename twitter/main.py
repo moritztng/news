43d96b1cfcae56e022eb4baf64a991bed9098e7c
Moritz Thüning <moritz.thuening@gmail.com>
2022-09-19 10:14:38 +0200

    No tweet

--- twitter/main.py
+++ twitter/main.py
@@ -34,5 +34,6 @@ def tweet(event_data, context):
             f"{twitter_username_text}"
             f"https://github.com/{repository['owner']}/{repository['name']}"
         )
-        tweet_id = tweepy_client.create_tweet(text=tweet_text, in_reply_to_tweet_id=tweet_id).data['id']
+        print(tweet_text)
+        #tweet_id = tweepy_client.create_tweet(text=tweet_text, in_reply_to_tweet_id=tweet_id).data['id']
     return ''
