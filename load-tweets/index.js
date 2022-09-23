88b081198cf66a76664962c5d82cf2bad1907682
Moritz Thüning <moritz.thuening@gmail.com>
2022-09-23 18:36:06 +0200

    Add load-tweets

---
+++ load-tweets/index.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch'
+
+export async function getTweets(eventData, context, callback) {
+  const startTime = new Date()
+  const endTime = new Date()
+
+  startTime.setMinutes(startTime.getMinutes() - 10)
+  endTime.setHours(endTime.getHours() - 24)
+
+  const body = {
+    recentSearch: {
+      query: process.env.QUERY,
+      maxResults: 100,
+      startTime: startTime.toISOString(),
+      endTime: endTime.toISOString(),
+      category: process.env.CATEGORY,
+      subCategory: process.env.SUB_CATEGORY,
+    },
+    dataSet: {
+      newDataSet: process.env.NEW_DATASET === 'true',
+      dataSetName: process.env.DATASET_NAME,
+    },
+  }
+
+  await fetch('https://news-361012.uc.r.appspot.com/search', {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' },
+  })
+
+  callback()
+}
